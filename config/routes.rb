3e1b207c2109563f0d7d1c480bbe2c8e5a168a3c
Denzyl Balram <denzyl.balram@gmail.com>
2015-10-22 16:21:53 -0700

    Start to fill inSessions Controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Myflix::Application.routes.draw do
 
   get 'ui(/:action)', controller: 'ui'
   get 'register', to: "users#new"
+  get 'sign_in', to: "sessions#new"
   resources :users, only: [:create]
 end
