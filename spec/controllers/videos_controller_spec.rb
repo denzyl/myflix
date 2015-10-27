34095bbc5e2829009095c4bfcb4e9fc0b5329435
Denzyl Balram <denzyl.balram@gmail.com>
2015-10-27 11:00:23 -0700

    Add spec file for Videos controller

---
+++ spec/controllers/videos_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe VideosController do
+  describe "GET show" do
+    it "sets @video" do
+      video = Fabricate(:video)
+      get :show, id: video.id
+      expect(assigns(:video)).to eq(video)
+    end
+
+
+    it "renders the show template"
+  end
+end
+
+set up
+action
+verify against result
