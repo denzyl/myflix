818409e5b9dc903ae32b3b32f713b489431ca3e6
Denzyl Balram <denzyl.balram@gmail.com>
2015-10-27 13:45:18 -0700

    Add User model and Fabricators

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -2,17 +2,17 @@ require 'spec_helper'
 
 describe VideosController do
   describe "GET show" do
-    it "sets @video" do
+    it "sets @video for authenticated users" do
+      session[:user_id] = Fabricate(:user).id
       video = Fabricate(:video)
       get :show, id: video.id
       expect(assigns(:video)).to eq(video)
     end
 
-
-    it "renders the show template"
+    it "redirects the user to the sign in page for unauthenticated users" do
+      video = Fabricate(:video)
+      get :show, id: video.id
+      expect(response).to redirect_to
+    end
   end
 end
-
-set up
-action
-verify against result
